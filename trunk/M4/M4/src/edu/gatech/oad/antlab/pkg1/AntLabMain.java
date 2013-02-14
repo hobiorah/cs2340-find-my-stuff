7ee815a87c6a89986b6b9ae48c0825e79fedd511
ueno.wataru@live.com <ueno.wataru@live.com@49cae177-2b33-871b-ad65-db6834d4040f>
2013-02-14 20:28:31 +0000

    added my name and num to printOutMessage method

--- trunk/M4/M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ trunk/M4/M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,8 +75,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("ggg004x");
 		  //Person5 replace P5 with your name
           //and ggg000x with your gt num
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("ggg005x");
+          Person5 p5 = new Person5("Wataru");
+          toPrint += p5.toString("902937699");
         System.out.println(toPrint);
         
         

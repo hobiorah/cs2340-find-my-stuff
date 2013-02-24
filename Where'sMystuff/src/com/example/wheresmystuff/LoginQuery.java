5eab918c931d9307638e23db369d298a1498f2c0
ThroughHymn@gmail.com <ThroughHymn@gmail.com@49cae177-2b33-871b-ad65-db6834d4040f>
2013-02-24 17:21:39 +0000

    fixed loginquery url and added functionality for login screen to try to actually log in

--- Where'sMystuff/src/com/example/wheresmystuff/LoginQuery.java
+++ Where'sMystuff/src/com/example/wheresmystuff/LoginQuery.java
@@ -6,29 +6,29 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class LoginQuery {
-	public LoginResult register(String username, String hash){
+	public LoginResult login(String username, String hash){
 		Document doc = null;
-		LoginResult reg = LoginResult.NETWORK_ERROR;
+		LoginResult log = LoginResult.NETWORK_ERROR;
 		
 		try {
-			doc = Jsoup.connect("http://steve.node13.info/findmystuff/createuser.php").data("name", username).data("hash", hash).get();
+			doc = Jsoup.connect("http://steve.node13.info/findmystuff/login.php").data("name", username).data("hash", hash).get();
 		} catch (IOException e) {
-			return reg;
+			return log;
 		}
 		
 		String[] array = doc.text().split(":");
 
 		if (!array[0].equals("valid"))
-			reg = LoginResult.INVALID;
+			log = LoginResult.INVALID;
 		else if (!array[1].equals("success"))
-			reg = LoginResult.DB_ERROR;
+			log = LoginResult.DB_ERROR;
 		else if (!array[2].equals("okuser"))
-			reg = LoginResult.INVALID;
+			log = LoginResult.INVALID;
 		else if (!array[3].equals("okpass"))
-			reg = LoginResult.INVALID;
+			log = LoginResult.INVALID;
 		else
-			reg = LoginResult.ACCEPTED;
+			log = LoginResult.ACCEPTED;
 		
-		return reg;
+		return log;
 	}
 }
--- Where'sMystuff/src/com/example/wheresmystuff/LoginScreen.java
+++ Where'sMystuff/src/com/example/wheresmystuff/LoginScreen.java
@@ -1,16 +1,27 @@
 package com.example.wheresmystuff;
 
+//import com.example.wheresmystuff.SignupPage.RegisterAttemptTask;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class LoginScreen extends Activity {
 
+	
+	TextView userName;
+	TextView password;
+	LoginQuery log;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +30,9 @@ public class LoginScreen extends Activity {
 		//login.setOnClickListener(new Login());
 		Button signUp = (Button)this.findViewById(R.id.signup_button);
 		//signUp.setOnClickListener(new SignUp());
+		userName = (TextView)this.findViewById(R.id.username_login);
+		password = (TextView)this.findViewById(R.id.password_login); 
+		log = new LoginQuery();
 	}
 
 	@Override
@@ -34,8 +48,72 @@ public class LoginScreen extends Activity {
 	 * @param view
 	 */
 	public void login(View view){
-		
+		new LoginAttemptTask().execute();
 	}
+	
+	private class LoginAttemptTask extends AsyncTask<Void, Void, LoginResult>{
+		private ProgressDialog pd;
+
+		protected void onPreExecute(){
+			pd = ProgressDialog.show(LoginScreen.this, null, "Logging in...", true);
+		}
+
+		protected LoginResult doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+
+			String user =  userName.getText().toString();
+			String pass =  password.getText().toString();
+			return   log.login(user, pass);//.register(user, pass);
+
+		}
+
+		protected void onPostExecute(LoginResult params){
+			pd.dismiss();
+
+			String x = "User created.";
+
+			switch(params){
+			case INVALID:
+				x = "Invalid Username";
+				break;
+			case DB_ERROR:
+				x = "Database Error";
+				break;
+			case NETWORK_ERROR:
+				x = "Network Error";
+				break;
+			}
+
+
+			if (params == LoginResult.ACCEPTED){
+				Toast.makeText(getApplicationContext(), x, Toast.LENGTH_LONG).show();
+				
+			} else {
+				popUp(x);
+				//finish();
+			}
+
+		}
+	}
+
+	public void popUp(CharSequence problem){
+		// 1. Instantiate an AlertDialog.Builder with its constructor
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		// 2. Chain together various setter methods to set the dialog characteristics
+		builder.setMessage("Please try again").setTitle(problem);
+
+		// Add the buttons
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				// User clicked OK button
+			}
+		});
+		// 3. Get the AlertDialog from create()
+		AlertDialog dialog = builder.create();
+		dialog.show();
+	}
+	
 	
 	public void signUp(View view){
 		 // Create the Intent

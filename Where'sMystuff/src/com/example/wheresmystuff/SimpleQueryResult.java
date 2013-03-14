c63a2bc414ea2636714e32d16c556ed84b7096c0
ThroughHymn@gmail.com <ThroughHymn@gmail.com@49cae177-2b33-871b-ad65-db6834d4040f>
2013-03-14 16:05:28 +0000

    deleted and added java docs

--- Where'sMystuff/src/com/example/wheresmystuff/SimpleQueryResult.java
+++ Where'sMystuff/src/com/example/wheresmystuff/SimpleQueryResult.java
@@ -1,6 +1,10 @@
 package com.example.wheresmystuff;
 
-
+/**
+ * if the submition of a item was successful
+ * @author HarryO
+ *
+ */
 public enum SimpleQueryResult {
 	OK, DB_ERROR, NETWORK_ERROR
 }

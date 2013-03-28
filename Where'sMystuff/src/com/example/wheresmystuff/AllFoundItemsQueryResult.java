1b17fcb6fc60e1fdc1a4a216aea075368edaa4a5
wufufufu@gmail.com <wufufufu@gmail.com@49cae177-2b33-871b-ad65-db6834d4040f>
2013-03-28 22:58:22 +0000

    added found item support

---
+++ Where'sMystuff/src/com/example/wheresmystuff/AllFoundItemsQueryResult.java
@@ -0,0 +1,10 @@
+package com.example.wheresmystuff;
+
+/**
+ * the results of the alllostitemsquery in a category 
+ * @author HarryO
+ *
+ */
+public enum AllFoundItemsQueryResult {
+	OK, EMPTY, DB_ERROR, NETWORK_ERROR
+}

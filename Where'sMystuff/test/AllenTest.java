1da908096f3dae1f1f6c439bae1f7ceeb1855adf
ueno.wataru@live.com <ueno.wataru@live.com@49cae177-2b33-871b-ad65-db6834d4040f>
2013-04-07 20:34:49 +0000

    updated tests including wataru Test

--- Where'sMystuff/test/AllenTest.java
+++ Where'sMystuff/test/AllenTest.java
@@ -29,5 +29,59 @@ public class AllenTest {
 		assertTrue(array[5].equalsIgnoreCase("admin"));
 		new DeleteUserQuery().delete(user);
 	}
+	RegisterQuery reger = new RegisterQuery();
+	DeleteUserQuery deleter = new DeleteUserQuery();
+	LoginQuery login = new LoginQuery();
+	
+	
+	@Test
+	public void testLoginValidUser() {
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		assertEquals(LoginResult.ACCEPTED, login.login("name", "password"));
+		
+		deleter.delete(user);
+	}
+	
+	@Test
+	public void testLoginInvalidUser() {
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		assertEquals(LoginResult.INVALID, login.login("namer", "password"));
 
+		
+		deleter.delete(user);	
+	}
+	@Test
+	public void testLoginInvalidPass() {
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		assertEquals(LoginResult.INVALID, login.login("name", "passwords"));
+
+		
+		deleter.delete(user);	}
+	@Test
+	public void testLoginLocked() {
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		login.login("name", "passwords");
+		login.login("name", "passwords");
+		login.login("name", "passwords");
+
+		assertEquals(LoginResult.LOCKED, login.login("name", "password"));
+		
+		deleter.delete(user);	}
+	
+	@Test
+	public void testLogInValidBoth() {
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		assertEquals(LoginResult.INVALID, login.login("names", "passwords"));
+		
+		deleter.delete(user);	}
 }

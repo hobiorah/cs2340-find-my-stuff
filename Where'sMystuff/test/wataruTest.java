140481d643ea9faf14e2bf9bb37509979d00d8bf
ueno.wataru@live.com <ueno.wataru@live.com@49cae177-2b33-871b-ad65-db6834d4040f>
2013-04-07 20:30:39 +0000

    updated test

--- Where'sMystuff/test/wataruTest.java
+++ Where'sMystuff/test/wataruTest.java
@@ -1,39 +1,72 @@
-import junit.framework.TestCase;
 import org.junit.Test;
 
+import junit.framework.TestCase;
+
+import android.test.*;
+
+
 import com.example.wheresmystuff.controller.DeleteUserQuery;
+import com.example.wheresmystuff.controller.LoginQuery;
+import com.example.wheresmystuff.controller.LoginResult;
 import com.example.wheresmystuff.controller.RegisterQuery;
+import com.example.wheresmystuff.model.User;
 
 import static org.junit.Assert.*;
 
 public class wataruTest {
 	RegisterQuery reger = new RegisterQuery();
 	DeleteUserQuery deleter = new DeleteUserQuery();
+	LoginQuery login = new LoginQuery();
 	
 	
 	@Test
 	public void testLoginValidUser() {
-		reger.register("name","password","hey@email.com");
-		assertEquals(2,edgeList.size());
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
 		
+		assertEquals(LoginResult.ACCEPTED, login.login("name", "password"));
 		
+		deleter.delete(user);
 	}
 	
 	@Test
 	public void testLoginInvalidUser() {
-		assertEquals(2, edgeList.size());
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		assertEquals(LoginResult.INVALID, login.login("namer", "password"));
+
+		
+		deleter.delete(user);	
 	}
 	@Test
 	public void testLoginInvalidPass() {
-		assertEquals(2, edgeList.size());
-	}
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		assertEquals(LoginResult.INVALID, login.login("name", "passwords"));
+
+		
+		deleter.delete(user);	}
 	@Test
-	public void testLoginValidPass() {
-		assertEquals(2, edgeList.size());
-	}
+	public void testLoginLocked() {
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		login.login("name", "passwords");
+		login.login("name", "passwords");
+		login.login("name", "passwords");
+
+		assertEquals(LoginResult.LOCKED, login.login("name", "password"));
+		
+		deleter.delete(user);	}
 	
 	@Test
-	public void testLogInValidUser() {
-		assertEquals(2, edgeList.size());
-	}
+	public void testLogInValidBoth() {
+		User user = new User("name","password","hey@email.com");
+		reger.register(user);
+		
+		assertEquals(LoginResult.INVALID, login.login("names", "passwords"));
+		
+		deleter.delete(user);	}
 }
